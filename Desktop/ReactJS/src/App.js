75bcd1cafd5d9f8866b3c3963bbad774abe9a498
PrathamDeora <prathamdevda112@gmail.com>
2025-02-20 14:11:39 +0530

    DONE

---
+++ Desktop/ReactJS/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./MyComponents/Header";
+import {Todos} from "./MyComponents/Todos";
+import Footer from "./MyComponents/Footer";
+
+function App() {
+  return (
+    <>
+    <Header title="My Todos List"/>
+    <Todos />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
